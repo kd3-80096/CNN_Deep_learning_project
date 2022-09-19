58ce22cfc50b83d9bbb2d79470f0ff5183d8c9fc
deveshpatil619 <75167794+deveshpatil619@users.noreply.github.com>
2022-09-19 14:00:09 +0530

    st 01 added

--- src/DeepClassifier/entity/config_entity.py
+++ src/DeepClassifier/entity/config_entity.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass  #This will help us create same structure as above namedtuple
+from dataclasses import (
+    dataclass,
+)  # This will help us create same structure as above namedtuple
 from pathlib import Path
 
 
-@dataclass(frozen=True)  ## 
+@dataclass(frozen=True)  ##
 class DataIngestionConfig:
-    root_dir: Path   ## here type will be path
-    source_URL: str     ## here type will be 
+    root_dir: Path  ## here type will be path
+    source_URL: str  ## here type will be
     local_data_file: Path
     unzip_dir: Path
