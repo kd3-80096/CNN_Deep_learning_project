4aeff9bd2929a5753ba942bf4667700f900bbaf8
deveshpatil619 <75167794+deveshpatil619@users.noreply.github.com>
2022-09-27 10:06:07 +0530

    stage 04 scripts

--- src/DeepClassifier/components/__init__.py
+++ src/DeepClassifier/components/__init__.py
@@ -2,7 +2,7 @@ from DeepClassifier.components.data_ingestion import DataIngestion
 from DeepClassifier.components.prepare_base_model import PrepareBaseModel
 from DeepClassifier.components.prepare_callback import PrepareCallback
 from DeepClassifier.components.training import Training
-from DeepClassifier.components.training import Evaluation
+from DeepClassifier.components.evaluation import Evaluation
 
 
 
