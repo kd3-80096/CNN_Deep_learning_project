d8710b4a7d56afa665e4340cad8f1f8928c36098
DEVESH PATIL <deveshpatil619@gmail.com>
2022-10-12 13:59:40 +0530

    configuration

--- src/DeepClassifier/config/configuration.py
+++ src/DeepClassifier/config/configuration.py
@@ -7,8 +7,9 @@ import os
 
 class ConfigurationManager:
     def __init__(
-        self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH
-    ):
+        self,
+        config_filepath=CONFIG_FILE_PATH,
+        params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
