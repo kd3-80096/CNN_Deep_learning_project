0689988fdf816f6fb6d0539a407ea7a80f40abd6
DEVESH PATIL <deveshpatil619@gmail.com>
2022-10-12 13:58:16 +0530

    Unit and Integration tests

---
+++ tests/unit/test_data_ingestion.py
@@ -0,0 +1,33 @@
+import pytest
+from DeepClassifier.entity import DataIngestionConfig
+from DeepClassifier.components import DataIngestion
+from pathlib import Path
+import os
+
+class Test_DataIngestion_download:
+    data_ingestion_config = DataIngestionConfig(
+        root_dir="tests/data/", 
+        source_URL="https://raw.githubusercontent.com/c17hawke/raw_data/main/sample_data.zip",  ## url of sample data
+        local_data_file="tests/data/data.zip",    
+        unzip_dir="tests/data/")
+
+    def test_download(self):
+        data_ingestion = DataIngestion(config=self.data_ingestion_config)
+        data_ingestion.download_file()
+        assert os.path.exists(self.data_ingestion_config.local_data_file)  ## to check if the local data file exists
+
+class Test_DataIngestion_unzip:
+    data_ingestion_config = DataIngestionConfig(root_dir="tests/data/",  ## creating the folder to store the sample images
+        source_URL="",
+        local_data_file ="tests/data/sample_data.zip",     ## zipping the data
+        unzip_dir="tests/data/")              ## unzipping the data
+
+    def test_unzip(self):
+        data_ingestion = DataIngestion(config=self.data_ingestion_config)
+        data_ingestion.unzip_and_clean()
+        assert os.path.isdir(Path("tests/data/PetImages"))
+        assert os.path.isdir(Path("tests/data/PetImages/Cat"))
+        assert os.path.isdir(Path("tests/data/PetImages/Dog"))  ## to check whether the files exists
+
+
+
